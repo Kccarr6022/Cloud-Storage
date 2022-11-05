f7cf45f2cb4340f16061a35e251cee1b1ca51076
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-11-04 21:15:00 -0400

    Backend and frontend link

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 import os
 
 db = SQLAlchemy()
 ma = Marshmallow()
+cors = CORS()
 
 def create_app():
     # Initialize application
@@ -30,7 +32,8 @@ def create_app():
     jwt = JWTManager(app)
 
     # init db and ma
-    db = SQLAlchemy(app)
-    ma = Marshmallow(app)
+    db.init_app(app)
+    ma.init_app(app)
+    cors.init_app(app)
 
     return app
--- backend/manage.py
+++ backend/manage.py
@@ -1,18 +1,11 @@
 def deploy():
     """Run deployment tasks."""
-    from app import create_app,db
-    from flask_migrate import upgrade,migrate,init,stamp
+    from app import create_app, db
     from models import User, Video, video_schema, videos_schema, user_schema, users_schema
 
     app = create_app()
-    app.app_context().push()
-
     # create database and tables
     db.create_all()
-
-    # migrate database to latest revision
-    stamp()
-    migrate()
-    upgrade()
+    
 
 deploy()
