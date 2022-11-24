fcad1ee0411b9c8d54b10fb837ae34c6bf23f164
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-11-23 20:13:06 -0500

    Code additions for serverless migration
    What is new?
    
    Added dependencies of backend for Python 3.8 environment.
    
    Flask==1.1.4
    Werkzeug==1.0.1
    markupsafe==2.0.1
    flask-sqlalchemy==2.4.4
    flask-marshmallow==0.14.0
    Flask-Cors==3.0.9
    python-dotenv==0.14.0
    SQLAlchemy==1.3.19
    Flask-JWT-Extended==3.24.0
    
    Additionally, removed the dependency Flask_migrate which does not
    cooperate with lambda functions

--- backend-serverless/models.py
+++ backend-serverless/models.py
@@ -1,24 +1,57 @@
-from flask import Flask, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-from flask_cors import CORS
-# from models import Users, Videos, video_schema, videos_schema, user_schema, users_schema
+###############################################
+# Models
+# --------------------
+# This file houses the definitions for all the database models via SQLAlchemy.
+#
+###############################################
 
+from sqlalchemy import ForeignKey
+from createapp import db,ma
 
+# Database classes
+class Users(db.Model):
+    id = db.Column(db.Text, primary_key=True) # authentication token
+    first_name = db.Column(db.String(100),nullable=False)
+    last_name = db.Column(db.String(100),nullable=True)
+    email = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.String(100),nullable=False)
 
-app = Flask(__name__)
+    def __rep__(self):
+        return f"Name: {self.first_name}, {self.last_name}"
 
+class Videos(db.Model):
+    id = db.Column(db.Text, ForeignKey("users.id"))
+    name = db.Column(db.Text, primary_key = True)
+    event_type = db.Column(db.Text, nullable=True)
+    duration = db.Column(db.Text, nullable=False)
+    fps = db.Column(db.Integer, nullable=True)
+    original_fps = db.Column(db.Integer, nullable=True)
+    date = db.Column(db.Text, nullable=False)
+    time = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Float, nullable= False)
+    width = db.Column(db.Integer, nullable = False)
+    height = db.Column(db.Integer, nullable = False)
+    url = db.Column(db.Text, nullable=False)
+    is_public = db.Column(db.Boolean, nullable=False, default=False)
 
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
+    def __rep__(self):
+        return f"Name: {self.name}, {self.event_type}"
 
+# Database schemas
+class VideosSchema(ma.Schema):
+    class Meta: # symptom number, symptom, symptom value
+        fields = ('id', 'name', 'event_type', 'duration', 'fps', 'original_fps', 'date', 'time', 'size', 'width', 'height', 'url')
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+class UsersSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'first_name', 'last_name', 'email', 'password')
 
-
-@app.errorhandler(404)
-def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+####################################
+#
+# Schema for Exporting
+#
+####################################
+video_schema = VideosSchema()
+videos_schema = VideosSchema(many=True)
+user_schema = UsersSchema()
+users_schema = UsersSchema(many=True)
