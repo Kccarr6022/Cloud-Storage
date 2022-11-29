1e17378e29f7d935d547bbbd83b0515e48cc77be
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-11-29 13:16:03 -0500

    Added video deletion prompt

--- src/components/VideoCard/index.js
+++ src/components/VideoCard/index.js
@@ -1,11 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Context } from '../../store/appContext'
 import './index.scss'
-import { Button } from '@material-ui/core'
 
 const VideoCard = props => {
   const { store } = useContext(Context)
   const [videoExists, setVideoExists] = useState(true)
+  const [prompt, setPrompt] = useState(null)
+
+  const handledelete = () => {
+    setPrompt('Would you like to delete this video')
+  }
+
+  const handleNo = () => {
+    setPrompt(null)
+  }
 
   const deleteVideo = async => {
     const opts = {
@@ -30,19 +38,31 @@ const VideoCard = props => {
   return (
     <>
       {videoExists ? (
-        <div id='videocard'>
-          <h1 className='video-name'>{props.video['name']}</h1>
-          <h1 className='video-type'>{props.video['event_type']}</h1>
-          <video autoPlay muted src={props.video['url']} width='200px' height='125px' />
-          <h1 className='video-date'>
-            {props.video['date']} {props.video['time']}
-          </h1>
-          <button>
-            <a href={props.video['url']}> Download </a>
-          </button>
-          <button>Watch</button>
-          <button onClick={deleteVideo}>Del</button>
-        </div>
+        prompt == null ? (
+          <div id='videocard'>
+            <h1 className='video-name'>{props.video['name']}</h1>
+            <h1 className='video-type'>{props.video['event_type']}</h1>
+            <video autoPlay muted src={props.video['url']} width='200px' height='125px' />
+            <h1 className='video-date'>
+              {props.video['date']} {props.video['time']}
+            </h1>
+            <button>
+              <a href={props.video['url']}> Download </a>
+            </button>
+            <button>Watch</button>
+            <button onClick={handledelete}>Del</button>
+          </div>
+        ) : (
+          <>
+            <div id='promptcard'>
+              <h1 className='prompt'>{prompt}</h1>
+
+              <button onClick={deleteVideo}>Yes</button>
+              <br />
+              <button onClick={handleNo}>No</button>
+            </div>
+          </>
+        )
       ) : (
         <></>
       )}
