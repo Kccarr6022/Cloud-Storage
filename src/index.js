89ff9fb58578bca81b94f9946122a6bf98a60e5a
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-12-01 16:49:03 -0500

    Working video background

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
-import App from "./App";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './index.scss'
+import App from './App'
+import backgroundVideo from './assets/background.mp4'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <video
+      autoPlay
+      loop
+      muted
+      id='video'
+      src={backgroundVideo}
+      type='video/mp4'
+    />
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+)
